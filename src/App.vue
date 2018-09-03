33c7de2c1adb7e4584203bf3461a0c066621e029
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2018-09-03 13:08:04 -0600

    Added router plugin

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <Login />
-    <Signup />
-    <Users />
-    <Navbar />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import Login from './components/Login'
-import Signup from './components/Signup'
-import Users from './components/Users'
-import Navbar from './components/Navbar'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-    Login,
-    Signup,
-    Users,
-    Navbar
-  }
-}
-</script>
 
 <style>
 #app {
